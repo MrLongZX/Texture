07d8c2b487e16144a551ae56f20b4587b9515737
苏友龙 <18366132400@163.com>
2018-08-05 22:07:55 +0800

    练习

--- examples/AsyncDisplayKitOverview/Sample/Node Containers/YLOverViewASCollectionNode.m
+++ examples/AsyncDisplayKitOverview/Sample/Node Containers/YLOverViewASCollectionNode.m
@@ -9,7 +9,7 @@
 #import "YLOverViewASCollectionNode.h"
 #import <AsyncDisplayKit/AsyncDisplayKit.h>
 
-@interface YLOverViewASCollectionNode()
+@interface YLOverViewASCollectionNode()<ASCollectionDelegate,ASCollectionDataSource>
 
 /** collection */
 @property (nonatomic, strong) ASCollectionNode *collectionNode;
@@ -18,6 +18,45 @@
 
 @implementation YLOverViewASCollectionNode
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        UICollectionViewFlowLayout *layout = [UICollectionViewFlowLayout new];
+        _collectionNode = [[ASCollectionNode alloc] initWithCollectionViewLayout:layout];
+        _collectionNode.delegate = self;
+        _collectionNode.dataSource = self;
+        [self addSubnode:_collectionNode];
+    }
+    return self;
+}
 
+- (ASLayoutSpec *)layoutSpecThatFits:(ASSizeRange)constrainedSize
+{
+    _collectionNode.style.width = ASDimensionMakeWithFraction(1.0);
+    _collectionNode.style.height = ASDimensionMakeWithFraction(1.0);
+    return [ASWrapperLayoutSpec wrapperWithLayoutElement:_collectionNode];
+}
+
+- (NSInteger)collectionNode:(ASCollectionNode *)collectionNode numberOfItemsInSection:(NSInteger)section
+{
+    return 50;
+}
+
+// 线程安全的
+- (ASCellNodeBlock)collectionNode:(ASCollectionNode *)collectionNode nodeBlockForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return ^{
+        ASTextCellNode *cellNode = [ASTextCellNode new];
+        cellNode.backgroundColor = UIColor.lightGrayColor;
+        cellNode.text = [NSString stringWithFormat:@"row : %ld",indexPath.row];
+        return cellNode;
+    };
+}
+
+- (ASSizeRange)collectionNode:(ASCollectionNode *)collectionNode constrainedSizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return ASSizeRangeMake(CGSizeMake(100, 100));
+}
 
 @end
